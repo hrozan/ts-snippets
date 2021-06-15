2ab0a709b77aedd6efda2b4fe9f7bf0e0ba3797f
hrozan <higorb.rozan@gmail.com>
2021-06-15 18:23:52 -0300

    feat: add chapter 1 ex 2

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,4 +3,5 @@ module.exports = {
 	singleQuote: true,
 	tabWidth: 4,
 	useTabs: true,
+	printWidth: 120,
 }
--- src/chapter-one/ex2/checkPermutation.ts
+++ src/chapter-one/ex2/checkPermutation.ts
@@ -1,7 +1,34 @@
-export const checkPermutation = (str: string, strCompare: string): boolean => {
-	for (const c of str) {
-		console.log(c)
-	}
+type StringHashTable = {
+	[key: number]: string[]
+}
 
-	throw new Error('Not Implemented')
+const stringToStringHashTable = (str: string): StringHashTable => {
+	const table: StringHashTable = {}
+	for (const c of str) {
+		const key = c.charCodeAt(0)
+		if (!table[key]) {
+			table[key] = [c]
+		} else {
+			table[key].push(c)
+		}
+	}
+	return table
+}
+
+const stringHashTableToString = (table: StringHashTable): string => {
+	let result = ''
+	for (const i of Object.values(table)) {
+		i.forEach((c) => (result = result.concat(c)))
+	}
+	return result
+}
+
+export const checkPermutation = (main: string, compare: string): boolean => {
+	if (main.length !== compare.length) {
+		return false
+	}
+	const stringHashTable = stringToStringHashTable(main)
+	const compareStringHashTable = stringToStringHashTable(compare)
+
+	return stringHashTableToString(stringHashTable) === stringHashTableToString(compareStringHashTable)
 }
