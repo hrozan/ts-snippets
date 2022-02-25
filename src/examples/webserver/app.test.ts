256f716896ec5793ad75b62c58e43cfe990f292e
Higor Rozan <higor.rozan@tc.com.br>
2022-02-25 14:21:06 -0300

    feat: add OO priciples to webserver

--- src/examples/webserver/app.test.ts
+++ src/examples/webserver/app.test.ts
@@ -1,13 +1,19 @@
-import { run, close } from "./app";
 import axios from "axios";
+import { App } from "./app";
 
-describe("app", function () {
-	afterEach(async function () {
-		await close();
+describe("app", () => {
+	let app: App;
+
+	beforeEach(() => {
+		app = new App();
+	});
+
+	afterEach(async () => {
+		return app.close();
 	});
 
 	test("should ", async function () {
-		await run();
+		await app.run();
 
 		const response = await axios.get("http://localhost:3000");
 
--- src/examples/webserver/app.ts
+++ src/examples/webserver/app.ts
@@ -1,32 +1,35 @@
-import { createServer, IncomingMessage, ServerResponse } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
-function listener(_request: IncomingMessage, response: ServerResponse) {
-	response.writeHead(200);
-	response.end("hello");
-}
+export class App {
+	server: Server;
+	constructor() {
+		this.server = createServer(this.listener);
+	}
 
-const server = createServer(listener);
+	private listener(_request: IncomingMessage, response: ServerResponse) {
+		response.writeHead(200);
+		response.end("hello");
+	}
+	run(): Promise<void> {
+		return new Promise((resolve, rejects) => {
+			this.server.on("error", () => {
+				rejects();
+			});
 
-export function run(): Promise<void> {
-	return new Promise((resolve, rejects) => {
-		server.on("error", () => {
-			rejects()
-		})
-
-		server.listen(3000, ()=>{
-			resolve()
-		})
-	})
-}
-
-export function close(): Promise<void> {
-	return new Promise((resolve, rejects) => {
-		server.close(err => {
-			if (err) {
-				rejects(err)
-			}
-			resolve()
+			this.server.listen(3000, () => {
+				resolve();
+			});
 		});
+	}
 
-	})
+	close(): Promise<void> {
+		return new Promise((resolve, rejects) => {
+			this.server.close((err) => {
+				if (err) {
+					rejects(err);
+				}
+				resolve();
+			});
+		});
+	}
 }
